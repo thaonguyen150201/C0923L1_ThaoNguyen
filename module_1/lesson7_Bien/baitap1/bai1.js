83bdabbdddbf9eade6b4bb1138cb8100757dd95c
unknown <huynhnguyen078848@gmail.com>
2023-10-23 21:27:03 +0700

    nop bai tap

---
+++ module_1/lesson7_Bien/baitap1/bai1.js
@@ -0,0 +1,8 @@
+let diemHoa = +prompt('Nhập điểm thi Hóa')
+let diemSinh = +prompt('(Nhập điểm thi Sinh')
+let diemLi = +prompt('Nhập điểm thi Lí')
+let tongDiem = diemHoa + diemSinh + diemLi
+let diemTrungBinh = tongDiem / 3
+document.write('diemTrungBinh=' + diemTrungBinh)
+document.write('<br>')
+document.write('TongDiem=' + TongDiem)
