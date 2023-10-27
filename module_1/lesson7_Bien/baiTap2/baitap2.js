2a9e19ab4d7493884e651df831f5a10de9494bb3
unknown <huynhnguyen078848@gmail.com>
2023-10-27 18:56:16 +0700

    nop bai

--- module_1/lesson7_Bien/baiTap2/baitap2.js
+++ module_1/lesson7_Bien/baiTap2/baitap2.js
@@ -3,10 +3,13 @@ function doiTien() {
     let from=document.getElementById('from').value;
     let to=document.getElementById(to).value;
     let result;
-if (from=='VND'&&to=='USD'){ result='result:'+(amount*23000)+'$';
+    document.write(
+    if (from=='VND'&&to=='USD'){ result='result:'+(amount*23000)+'$';
+    }
+    else if(from=='USD'&&to=='VND'){result='result:'+(amount/23000)+'VND';
+    }
+    else if(from=='VND'&& to=='VND'){result='result'+amount+'VND'};
+else if(from=='USD'&& to=='USD'){result='result'+amount+'USD'}
+    document.getElementById('result').innerHTML=result
 }
-else if(from=='USD'&&to=='VND'){result='result:'+(amount/23000)+'VND';
-}
-
-document.getElementById('result').innerHTML=result
-}
+)
